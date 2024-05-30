9ae635df751990ae543619ef01088c07ba39aeec
Alex Zhidkov <alex.z@mate.academy>
2024-05-30 12:27:03 +0300

    [CI/CD]: update code and add gitignore

--- ci-cd/github-actions-ci-for-python-app/src/app.py
+++ ci-cd/github-actions-ci-for-python-app/src/app.py
@@ -14,13 +14,14 @@ appEnv = os.environ.get("APP_ENV", "Development")
 connectionString = os.environ.get("DB_CONNECTION", "")
 
 # Configure the MySQL database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = connectionString
+app.config["SQLALCHEMY_DATABASE_URI"] = connectionString
 db = SQLAlchemy(app)
 
 # Define a Counter model
 class Counter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Integer)
+
 
 # Initialize the databased
 with app.app_context():
@@ -31,7 +32,8 @@ with app.app_context():
         db.session.add(counter)
         db.session.commit()
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     counter = Counter.query.first()
     counter.value += 1
@@ -49,31 +51,38 @@ def hello():
 <pre>         \\____\\______/</pre>
     '''
 
-@app.route('/logo')
-def docker_logo():
-    return send_file('docker-logo.png', mimetype='image/png')
 
-@app.route('/health')
+@app.route("/logo")
+def docker_logo():
+    return send_file("docker-logo.png", mimetype="image/png")
+
+
+@app.route("/health")
 def health_check():
     return Response("Healthy", status=200)
 
-@app.route('/ready')
+
+@app.route("/ready")
 def readiness_check():
     if time.time() < start_time + start_period:
         return Response("Not Ready", status=503)
     else:
         return Response("Ready", status=200)
 
-@app.route('/external-call')
+
+@app.route("/external-call")
 def external_call():
-    external_url = os.getenv('EXTERNAL_ENDPOINT')
+    external_url = os.getenv("EXTERNAL_ENDPOINT")
     if not external_url:
         Response("EXTERNAL_ENDPOINT not defined", status=500)
     try:
         response = requests.get(external_url)
-        return Response(f"Extarnal call response: {response.text}", status = response.status_code)
+        return Response(
+            f"Extarnal call response: {response.text}", status=response.status_code
+        )
     except Exception as e:
         return Response(f"Error calling external endpoint: {str(e)}", status=500)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
